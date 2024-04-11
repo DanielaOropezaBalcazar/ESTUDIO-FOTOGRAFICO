9eacf57522467402d2dcbafcf31b817ce1704a22
natis25 <150816023+natis25@users.noreply.github.com>
2024-04-10 22:33:24 -0400

    Update header.php

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
     </ul>
     <ul class="navbar-nav ml-auto">
       <li class="nav-item">
-        <a class="nav-link" href="galeria.php">Fotos</a>
+        <a class="nav-link" href="fotos.php">Fotos</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="pedidos.php">Pedidos</a>
